35f6bc34f292ad8e3d0e6db65fca4cc0972ccaf0
Italo Fortes <152583162+ItaloFortes@users.noreply.github.com>
2023-12-13 20:11:34 -0300

    portifolio-landing

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Ocultar o loader e mostrar o conteúdo quando o site estiver carregado
+    document.body.classList.add("loaded");
+  });
